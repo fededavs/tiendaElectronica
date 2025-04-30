1def329462a467a71b1409e4bf12236057b0a570
fededavs <federicodavid94@gmail.com>
2025-04-30 11:47:23 -0300

    Se logro centrar el cart-container del carrito de compras

--- paginas/carrito.js
+++ paginas/carrito.js
@@ -9,23 +9,23 @@ function actualizar() {
 
     //Mostrar los productos en el carrito
     cart.forEach((item, index) => {
-        const itemDiv = document.createElement('div');
+        const itemDiv = document.createElement('div')
 
         // Crear el texto del producto
         const itemText = document.createElement('span');
         itemText.textContent = `${item.name} - $${item.price.toLocaleString()}`
-        itemDiv.appendChild(itemText);
+        itemDiv.appendChild(itemText)
 
         const deleteButton = document.createElement('button')
         deleteButton.textContent = 'Eliminar'
         deleteButton.classList.add('remove-button')
         deleteButton.addEventListener('click', () => eliminarDelCarrito(index))
 
-        itemDiv.appendChild(deleteButton);
-        cartItemsContainer.appendChild(itemDiv);
+        itemDiv.appendChild(deleteButton)
+        cartItemsContainer.appendChild(itemDiv)
 
         total += item.price; //Suma el precio del producto
-    });
+    })
 
     // Actualizar el total
     totalElementos.textContent = `Total: $${total.toLocaleString()}`
